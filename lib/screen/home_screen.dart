0aa021a61c2b39850ec95e2ef57cd36abc7480c6
Felipe Baz <fbazmitsuishi@gmail.com>
2023-02-13 23:39:10 -0300

    feat: add TabBar

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,17 +9,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var _selectedIndex = 2;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromRGBO(18, 20, 29, 1),
-      body: Center(
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(18, 20, 29, 1),
+      body: const Center(
         child: Text(
             'Home Page',
           style: TextStyle(
             color: Color.fromRGBO(224, 224, 224, 1)
           ),
         ),
+      ),
+      bottomNavigationBar: FlashyTabBar(
+        animationCurve: Curves.linear,
+        selectedIndex: _selectedIndex,
+        showElevation: true,
+        onItemSelected: (index) => setState(() {
+          _selectedIndex = index;
+        }),
+        items: [
+          FlashyTabBarItem(
+            icon: const Icon(Icons.search),
+            title: const Text('Metas'),
+          ),
+          FlashyTabBarItem(
+            icon: const Icon(Icons.highlight),
+            title: const Text('Trasações'),
+          ),
+          FlashyTabBarItem(
+            icon: const Icon(Icons.home),
+            title: const Text('Home'),
+          ),
+          FlashyTabBarItem(
+            icon: const Icon(Icons.settings),
+            title: const Text('Notificações'),
+          ),
+          FlashyTabBarItem(
+            icon: const Icon(Icons.settings),
+            title: const Text('Settings'),
+          ),
+        ],
       ),
     );
   }
