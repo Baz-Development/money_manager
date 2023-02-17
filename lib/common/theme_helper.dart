153703c750797b3acb87aff84cdb28e8e3ca3ff0
Felipe Baz <fbazmitsuishi@gmail.com>
2023-02-16 21:38:22 -0300

    fix: Floation text from text field bad visibility

--- lib/common/theme_helper.dart
+++ lib/common/theme_helper.dart
@@ -8,9 +8,7 @@ class ThemeHelper{
     return InputDecoration(
       labelText: lableText,
       hintText: hintText,
-      floatingLabelStyle: TextStyle(
-        color: HexColor("9D9D9E")
-      ),
+      floatingLabelBehavior: FloatingLabelBehavior.never,
       fillColor: Colors.white,
       filled: true,
       contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
